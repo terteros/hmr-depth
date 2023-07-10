344dbaeb602438e07114b380e73be5138cc7df96
batuhan <terteros@github.com>
2023-07-10 15:52:58 +0300

    Reverted back to raytune for multiple trials.

--- experiment.py
+++ experiment.py
@@ -1,24 +1,27 @@
 import argparse
+import os
+from functools import partial
+
+import ray
+import torch
 
 import wandb
 from pytorch_lightning.callbacks import ModelCheckpoint
+from ray import tune
 
 from lib.cfg import update_cfg
 from lib.models.hmr import HMR
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--cfg', type=str, help='cfg file path', required=True)
-parser.add_argument('--exp_name', type=str, help='cfg file path', required=True)
-args = parser.parse_args()
-base_config = update_cfg(args.cfg)
 
 
-def single_experiment():
+
+def single_experiment(config, base_config, wd, name):
+    os.chdir(wd)
     wandb.init(project='DDA')
     sweep_config_list = []
-    for k, v in wandb.config.items():
+    for k, v in config.items():
         sweep_config_list.append(k)
         sweep_config_list.append(v)
     exp_config = base_config.clone()
@@ -27,7 +30,7 @@ def single_experiment():
     if exp_config.SEED_VALUE >= 0:
         pl.seed_everything(exp_config.SEED_VALUE, workers=True)
 
-    wandblogger = WandbLogger(name=args.exp_name, project='DDA', log_model=True)
+    wandblogger = WandbLogger(name=name, project='DDA', log_model=True)
     wandblogger.log_hyperparams(exp_config)
 
     checkpoint_callback = ModelCheckpoint(
@@ -52,17 +55,30 @@ def single_experiment():
 
 
 if __name__ == '__main__':
-    sweep_config = {
-        'method': 'grid',
-        'name': args.exp_name,
-        'metric': {
-            'goal': 'minimize',
-            'name': f'val_{base_config.DATASET.VAL.SET[0]}/val_loss'
-        },
-        'parameters': {
-            "SEED_VALUE": {'values': [1, 2, 3]}
-        }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, help='cfg file path', required=True)
+    parser.add_argument('--exp_name', type=str, help='cfg file path', required=True)
+    args = parser.parse_args()
+    base_config = update_cfg(args.cfg)
+    exp_config = {
+        "SEED_VALUE": tune.grid_search([
+            56249,
+            # 921758,
+            # 486239,
+            # 358053,
+            # 173094,
+            # 17393,
+            # 4,
+            # 5
+        ]),
     }
-    single_experiment()
-    # sweep_id = wandb.sweep(sweep_config, project="DDA")
-    # wandb.agent(sweep_id=sweep_id, function=single_experiment, count=3)
+    ray.init(num_gpus=torch.cuda.device_count())
+    analysis = tune.run(
+        partial(single_experiment,base_config=base_config, wd=os.getcwd(), name=args.exp_name),
+        config=exp_config,
+        metric=None,
+        mode='min',
+        num_samples=1,
+        name=args.exp_name,
+        resources_per_trial={'cpu': 16, 'gpu': 1}
+    )
